850ed03d36e19d43b23c46a3e09882b06495c228
abdourhamane <abdouramanesoumaila@yahoo.fr>
2018-05-09 16:39:44 +0200

    compute the number of sms to send by collecting the amout of character typed

--- mpush-services/src/main/mpush-webapp-client/src/app/message/nouveau/nouveau.component.ts
+++ mpush-services/src/main/mpush-webapp-client/src/app/message/nouveau/nouveau.component.ts
@@ -9,12 +9,21 @@ import {UserService} from "../../core/user/user.service";
 })
 export class NouveauComponent implements OnInit {
   currentUser : User;
+  totalChar : number = 0; //Nombre total de caractères du message saisi
+  nbSMS : number = 0; //Nombre de SMS à facturer
+  message = ""; //Le message à envoyé
 
   constructor(public userService: UserService) {
     this.userService.getCurrentUser().then(user => this.currentUser = user);
   }
 
   ngOnInit() {
+  }
+
+  onKeyUp(event: any) {
+    event.preventDefault();
+    this.totalChar = this.message.length;
+    this.nbSMS = Math.ceil(this.totalChar / 160);
   }
 
 }
